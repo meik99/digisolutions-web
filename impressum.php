bbe361a02ba0849663863c5c86e0cdeba3df72df
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2018-08-12 04:02:44 +0200

    Implemented Impressum

---
+++ impressum.php
@@ -0,0 +1,366 @@
+<div class="row">
+    <div class="col-12">
+        <h1>Impressum</h1>
+    </div>
+</div>
+<hr>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        digisolutions<br>
+        Detlef Mayrhofer<br>
+        Heideweg 13,<br>
+        4225 Luftenberg,<br>
+        Österreich<br>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Unternehmensgegenstand: IT Dienstleistungen
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Tel.: 0699 / 110 44 373<br>
+        E-Mail: d.mayrhofer@digisolutions.at
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Berufsrecht: Gewerbeordnung: www.ris.bka.gv.at
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Aufsichtsbehörde/Gewerbebehörde: Bezirkshauptmannschaft Perg<br>
+        Verleihungsstaat: Österreich
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Quelle: Erstellt mit dem Impressum Generator von firmenwebseiten.at in Kooperation mit smartetechnik.at
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>EU-Streitschlichtung</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Gemäß Verordnung über Online-Streitbeilegung in Verbraucherangelegenheiten (ODR-Verordnung) möchten wir
+        Sie über die Online-Streitbeilegungsplattform (OS-Plattform) informieren.
+        Verbraucher haben die Möglichkeit, Beschwerden an die Online Streitbeilegungsplattform der
+        Europäischen Kommission unter http://ec.europa.eu/odr zu richten. Die dafür notwendigen
+        Kontaktdaten finden Sie oberhalb in unserem Impressum.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir möchten Sie jedoch darauf hinweisen, dass wir nicht bereit oder verpflichtet sind, an
+        Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>Haftung für Inhalte dieser Webseite</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle
+        Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte
+        auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wurden.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und
+        aktuelle Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit
+        aller Inhalte auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wurden.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>Haftung für Links auf dieser Webseite</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Unsere Webseite enthält Links zu anderen Webseiten für deren Inhalt wir nicht verantwortlich sind.
+        Haftung für verlinkte Websites besteht laut § 17 ECG für uns nicht, da wir keine Kenntnis rechtswidriger
+        Tätigkeiten hatten und haben, uns solche Rechtswidrigkeiten auch bisher nicht aufgefallen sind
+        und wir Links sofort entfernen würden, wenn uns Rechtswidrigkeiten bekannt werden.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Ihnen rechtswidrige Links auf unserer Website auffallen, bitte wir Sie uns zu kontaktieren,
+        Sie finden die Kontaktdaten im Impressum.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>Urheberrechtshinweis</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht.
+        Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>Bildernachweis</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.<br>
+        Die Bilderrechte liegen bei den folgenden Fotografen und Unternehmen:<br>
+        digisolutions
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h2>Datenschutzerklärung</h2>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h3>Datenschutz</h3>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir haben diese Datenschutzerklärung (Fassung 12.08.2018-221070141) verfasst,
+        um Ihnen gemäß der Vorgaben der Datenschutz-Grundverordnung (EU) 2016/679 und dem
+        Datenschutzgesetz (DSG) zu erklären, welche Informationen wir sammeln, wie wir Daten verwenden
+        und welche Entscheidungsmöglichkeiten Sie als Besucher dieser Webseite haben.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Leider liegt es in der Natur der Sache, dass diese Erklärungen sehr technisch klingen,
+        wir haben uns bei der Erstellung jedoch bemüht die wichtigsten Dinge so einfach und klar
+        wie möglich zu beschreiben.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h3>Automatische Datenspeicherung</h3>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Sie heutzutage Webseiten besuchen, werden gewisse Informationen automatisch
+        erstellt und gespeichert, so auch auf dieser Webseite. Wenn Sie unsere Webseite
+        so wie jetzt gerade besuchen, speichert unser Webserver (Computer auf dem diese
+        Webseite gespeichert ist) automatisch Daten wie
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                die Adresse (URL) der aufgerufenen Webseite
+            </li>
+            <li class="list-group-item">
+                Browser und Browserversion
+            </li>
+            <li class="list-group-item">
+                das verwendete Betriebssystem
+            </li>
+            <li class="list-group-item">
+                die Adresse (URL) der zuvor besuchten Seite (Referrer URL)
+            </li>
+            <li class="list-group-item">
+                den Hostname und die IP-Adresse des Geräts von welchem aus zugegriffen wird
+            </li>
+            <li class="list-group-item">
+                Datum und Uhrzeit
+            </li>
+            <li class="list-group-item">
+                in Dateien (Webserver-Logfiles).
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        In der Regel werden Webserver-Logfiles zwei Wochen gespeichert und danach automatisch gelöscht.
+        Wir geben diese Daten nicht weiter, können jedoch nicht ausschließen,
+        dass diese Daten beim Vorliegen von rechtswidrigem Verhalten eingesehen werden.
+        Die Rechtsgrundlage besteht nach Artikel 6  Absatz 1 f DSGVO (Rechtmäßigkeit der Verarbeitung)
+        darin, dass berechtigtes Interesse daran besteht, den fehlerfreien Betrieb dieser Webseite durch
+        das Erfassen von Webserver-Logfiles zu ermöglichen.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h3>Rechte laut Datenschutzgrundverordnung</h3>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Ihnen stehen laut den Bestimmungen der DSGVO und des österreichischen Datenschutzgesetzes
+        (DSG) grundsätzlich die folgende Rechte zu:
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                Recht auf Berichtung (Artikel 16 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Recht auf Löschung („Recht auf Vergessenwerden“) (Artikel 17 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Recht auf Einschränkung der Verarbeitung (Artikel 18 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Recht auf Benachrichtigung – Mitteilungspflicht im Zusammenhang mit der
+                Berichtigung oder Löschung personenbezogener Daten oder
+                der Einschränkung der Verarbeitung (Artikel 19 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Recht auf Datenübertragbarkeit (Artikel 20 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Widerspruchsrecht (Artikel 21 DSGVO)
+            </li>
+            <li class="list-group-item">
+                Recht, nicht einer ausschließlich auf einer automatisierten Verarbeitung
+                — einschließlich Profiling — beruhenden Entscheidung unterworfen zu werden (Artikel 22 DSGVO)
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt
+        oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind,
+        können Sie sich bei der Aufsichtsbehörde beschweren,
+        welche in Österreich die Datenschutzbehörde ist,
+        deren Webseite Sie unter https://www.dsb.gv.at/ finden.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h3>TLS-Verschlüsselung mit https</h3>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir verwenden https um Daten abhörsicher im Internet zu übertragen (Datenschutz
+        durch Technikgestaltung Artikel 25 Absatz 1 DSGVO). Durch den Einsatz von TLS
+        (Transport Layer Security), einem Verschlüsselungsprotokoll zur sicheren Datenübertragung
+        im Internet können wir den Schutz vertraulicher Daten sicherstellen. Sie erkennen die
+        Benutzung dieser Absicherung der Datenübertragung am kleinen Schloßsymbol links oben im Browser und der
+        Verwendung des Schemas https (anstatt http) als Teil unserer Internetadresse.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h3>Cookies</h3>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Unsere Website verwendet HTTP-Cookies um nutzerspezifische Daten zu speichern.
+        Ein Cookie ist ein kurzes Datenpaket, welches zwischen Webbrowser und Webserver ausgetauscht wird,
+        für diese aber völlig bedeutungslos ist und erst für die Webanwendung,
+        z. B. einen Online-Shop, eine Bedeutung erhält, etwa den Inhalt eines virtuellen Warenkorbes.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Es gibt zwei Arten von Cookies: Erstanbieter-Cookies werden von unserer Website erstellt,
+        Drittanbieter-Cookies werden von anderen Websites (z. B. Google Analytics) erstellt.
+        Man unterscheidet drei Kategorien von Cookies: unbedingt notwendige Cookies
+        um grundlegende Funktionen der Website sicherzustellen, funktionelle Cookies um die Leistung
+        der Webseite sicherzustellen und zielorientierte Cookies um das Benutzererlebnis zu verbessern.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wir nutzen Cookies, um unsere Webseite nutzerfreundlicher zu gestalten.
+        Einige Cookies bleiben auf Ihrem Endgerät gespeichert,
+        bis Sie diese löschen. Sie ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <h4>Cookie Einstellungen sehen und Cookies löschen</h4>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Sie feststellen möchten, welche Cookies in Ihrem Browser gespeichert wurden,
+        Cookie-Einstellungen ändern oder Cookies löschen möchten,
+        können Sie dies in Ihren Browser-Einstellungen finden:
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                Safari: Verwalten von Cookies und Websitedaten mit Safari
+            </li>
+            <li class="list-group-item">
+                Firefox: Cookies löschen, um Daten zu entfernen, die Websites auf Ihrem Computer abgelegt haben
+            </li>
+            <li class="list-group-item">
+                Chrome: Cookies in Chrome löschen, aktivieren und verwalten
+            </li>
+            <li class="list-group-item">
+                Internet Explorer: Löschen und Verwalten von Cookies
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Sie die Speicherung von Daten in Cookies nicht wünschen,
+        so können Sie Ihren Browser so einrichten, dass er Sie über das
+        Setzen von Cookies informiert und Sie dies nur im Einzelfall erlauben.
+        Sie können jederzeit Cookies, die sich bereits auf Ihrem Computer befinden,
+        löschen oder Cookies deaktivieren. Die Vorgangsweise dazu ist nach Browser
+        unterschiedlich, am besten Sie suchen die Anleitung in Google mit dem
+        Suchbegriff “Cookies löschen Chrome” oder „Cookies deaktivieren Chrome“
+        im Falle eines Chrome Browsers oder tauschen das Wort „Chrome“
+        gegen den Namen Ihres Browsers, z. B. Edge, Firefox, Safari aus.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Wenn Sie uns generell nicht gestatten, Cookies zu nutzen,
+        d.h. diese per Browsereinstellung deaktivieren,
+        können manche Funktionen und Seiten nicht wie erwartet funktionieren.
+    </div>
+</div>
+<div class="row text-justify">
+    <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        Quelle: Erstellt mit dem Datenschutz Generator von firmenwebseiten.at in Kooperation mit hashtagmann.de
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
